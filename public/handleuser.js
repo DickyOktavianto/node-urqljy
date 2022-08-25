80513ab777a5d34352cc95ecde8580a735d78252
DickyOktavianto <dickyoktavianto5@gmail.com>
2022-08-25 23:34:33 +0700

    3 files modified

--- public/handleuser.js
+++ public/handleuser.js
@@ -1,27 +1,3 @@
-// tangkap element form dengan id user form
-let user_form = document.getElementById('user_form');
+# node-urqljy
 
-// tambahkan eventlistener
-user_form.addEventListener('submit', function (event) {
-  // stop form dari reload
-  event.preventDefault();
-
-  // buat sebagai object sebagai wadah
-  let userData = {
-    ussername: '',
-    email: '',
-    password: '',
-    phone: '',
-    married: '',
-    addres: '',
-  };
-
-  userData.ussername = event.target.ussername.value;
-  userData.email = event.target.email.value;
-  userData.password = event.target.password.value;
-  userData.phone = event.target.phone.value;
-  userData.married = event.target.married.value;
-  userData.addres = event.target.addres.value;
-
-  console.info(userData);
-});
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-urqljy)
